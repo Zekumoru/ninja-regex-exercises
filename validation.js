bca5313ecbfbc284489790a62d06bb8e750a691b
Zekumoru Dragonhart <dev@zekumoru.com>
2022-07-07 16:27:19 +0200

    Write regex for email

--- validation.js
+++ validation.js
@@ -2,13 +2,15 @@ const inputs = document.querySelectorAll('input');
 
 const patterns = {
     username: /^[a-z\d]{5,12}$/i,
+    email: /^[a-z\d\.-]+@[a-z\d-]+\.[a-z]{2,8}(\.[a-z]{2,8})?$/i,
     password: /^[\w@-]{8,20}$/i,
     telephone: /^\d{11}$/,
     slug: /^[a-z\d-]{8,20}$/
 };
 
 function validate(field, regex) {
-    if (regex.test(field.value)) {
+    if (field.value && regex.test(field.value)) {
+        console.log(field.value);
         field.className = 'valid';
     }
     else {
