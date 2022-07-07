58e54cb62101e5a2ef86c446ad3c61c3d2170990
Zekumoru Dragonhart <dev@zekumoru.com>
2022-07-07 15:56:20 +0200

    Add visual feedback on input

--- validation.js
+++ validation.js
@@ -1,4 +1,20 @@
-// validation script here
+const inputs = document.querySelectorAll('input');
+
 const patterns = {
-        telephone: /^\d{11}$/
+    telephone: /^\d{11}$/
 };
+
+function validate(field, regex) {
+    if (regex.test(field.value)) {
+        field.className = 'valid';
+    }
+    else {
+        field.className = 'invalid';
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener('keyup', (e) => {
+        validate(e.target, patterns[e.target.attributes.name.value]);
+    })
+});
