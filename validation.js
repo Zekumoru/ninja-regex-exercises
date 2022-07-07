27f10884c57d426db6f750a8d13c65848468eb80
Zekumoru Dragonhart <dev@zekumoru.com>
2022-07-07 16:07:06 +0200

    Write regex for username, password, and slug

--- validation.js
+++ validation.js
@@ -1,7 +1,10 @@
 const inputs = document.querySelectorAll('input');
 
 const patterns = {
-    telephone: /^\d{11}$/
+    username: /^[a-z\d]{5,12}$/i,
+    password: /^[\w@-]{8,20}$/i,
+    telephone: /^\d{11}$/,
+    slug: /^[a-z\d-]{8,20}$/
 };
 
 function validate(field, regex) {
